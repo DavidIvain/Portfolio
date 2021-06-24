8ba29a0013b53702c863ad71e7ae98f90c68ee41
David Ivain <d.ivain@outlook.fr>
2021-06-24 16:19:47 +0200

    Header Component

---
+++ src/components/Header/Header.stories.tsx
@@ -0,0 +1,20 @@
+import { Meta, Story } from "@storybook/react";
+import { Header, HeaderProps } from "./Header";
+
+export default {
+    title: "Header",
+    component: Header
+} as Meta;
+
+const Template: Story<HeaderProps> = (args) => <Header {...args}></Header>
+
+export const Default = Template.bind({});
+Default.args = {
+    active: 0,
+    links: [
+        { text: "Home", href: "#" },
+        { text: "Two", href: "#" },
+        { text: "Three", href: "#" }
+    ],
+    avatarLink: "https://resize.programme-television.ladmedia.fr/rcrop/1200,/img/var/premiere/storage/images/tele-7-jours/news-tv/un-apres-midi-100-shrek-sur-france-2-4393382/89186312-1-fre-FR/Un-apres-midi-100-Shrek-sur-France-2.png"
+}
---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,25 @@
+import { Avatar } from "../Avatar/Avatar";
+import { Card } from "../Card/Card";
+import { Link, NavBar } from "../NavBar/NavBar";
+import "./header.scss";
+
+export const HEADER_ID = "header";
+
+export type HeaderProps = {
+    avatarLink: string;
+    links: Link[];
+    active?: string | number;
+};
+
+export const Header = (props: HeaderProps) => {
+    function changeLocale() { }
+    return <header id="header">
+        <div>
+            <Avatar src={props.avatarLink} alt="My Photo" size={160} />
+            <Card>
+                <h1>David IVAIN</h1>
+            </Card>
+        </div>
+        <NavBar links={props.links} active={props.active} />
+    </header>;
+}
