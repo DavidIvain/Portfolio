a87baf37ee832c0a29743ea7452b96cedfeb56a1
David Ivain <d.ivain@outlook.fr>
2021-06-30 13:20:49 +0200

    changed photo in header

--- src/components/Header/Header.stories.tsx
+++ src/components/Header/Header.stories.tsx
@@ -16,5 +16,6 @@ Default.args = {
         { text: "Two", href: "#" },
         { text: "Three", href: "#" }
     ],
-    avatarLink: "https://resize.programme-television.ladmedia.fr/rcrop/1200,/img/var/premiere/storage/images/tele-7-jours/news-tv/un-apres-midi-100-shrek-sur-france-2-4393382/89186312-1-fre-FR/Un-apres-midi-100-Shrek-sur-France-2.png"
+    //avatarLink: "https://resize.programme-television.ladmedia.fr/rcrop/1200,/img/var/premiere/storage/images/tele-7-jours/news-tv/un-apres-midi-100-shrek-sur-france-2-4393382/89186312-1-fre-FR/Un-apres-midi-100-Shrek-sur-France-2.png"
+    avatarLink: "/assets/avatar.jpg"
 }
